7a0b09c8040ede4b18b0f49abb718f82d5227e79
ramcharan <56796692+ramcharan10@users.noreply.github.com>
2021-09-01 19:33:24 +0700

    Update CustomerFactory.java

--- src/CustomerFactory.java
+++ src/CustomerFactory.java
@@ -2,13 +2,13 @@ public class CustomerFactory {
 
     public static Customer create(String type, String name, String address, String iD, String phone) {
         switch (type) {
-            case "guest": {
+            case "guest" -> {
                 return new GuestCustomer(name, address, iD, phone);
             }
-            case "vip": {
+            case "vip" -> {
                 return new VIPCustomer(name, address, iD, phone);
             }
-            case "supervip": {
+            case "supervip" -> {
                 return SuperVip.getInstance();
             }
         }
